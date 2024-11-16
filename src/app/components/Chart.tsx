2ce382ee23a962a44a25242c8988bba5445fe274
orazbek <oraz.daurenov@gmail.com>
2024-11-16 21:22:31 +0100

    refactor chart component into own file

--- src/app/components/Chart.tsx
+++ src/app/components/Chart.tsx
@@ -1,7 +1,45 @@
+"use client";
 import React from "react";
+import { ChartConfig, ChartContainer } from "../../components/ui/chart";
+import { Bar, BarChart } from "recharts";
 
 const Chart = () => {
-  return <div>Chart</div>;
+  const chartData = [
+    { month: "January", desktop: 50, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ];
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#2563eb",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#60a5fa",
+    },
+  } satisfies ChartConfig;
+
+  function Component() {
+    return (
+      <ChartContainer config={chartConfig} className="min-h-[300px] w-full">
+        <BarChart accessibilityLayer data={chartData}>
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        </BarChart>
+      </ChartContainer>
+    );
+  }
+
+  return (
+    <div className="">
+      <Component />
+    </div>
+  );
 };
 
 export default Chart;
--- src/app/components/Overview.tsx
+++ src/app/components/Overview.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { ChartConfig, ChartContainer } from "../../components/ui/chart";
-import { Bar, BarChart } from "recharts";
 import React from "react";
+import Chart from "./Chart";
 
 const Overview = () => {
   return (
@@ -17,48 +16,9 @@ export default Overview;
 
 const Title = () => {
   return (
-    <h1 className="text-dark-brown text-lg font-bold">
+    <h1 className="text-lg font-bold text-dark-brown">
       Spending - Last 7 days
     </h1>
-  );
-};
-
-const Chart = () => {
-  const chartData = [
-    { month: "January", desktop: 50, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-  ];
-
-  const chartConfig = {
-    desktop: {
-      label: "Desktop",
-      color: "#2563eb",
-    },
-    mobile: {
-      label: "Mobile",
-      color: "#60a5fa",
-    },
-  } satisfies ChartConfig;
-
-  function Component() {
-    return (
-      <ChartContainer config={chartConfig} className="min-h-[300px] w-full">
-        <BarChart accessibilityLayer data={chartData}>
-          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-        </BarChart>
-      </ChartContainer>
-    );
-  }
-
-  return (
-    <div className="">
-      <Component />
-    </div>
   );
 };
 
