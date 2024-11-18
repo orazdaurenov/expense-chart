94d2c9b36eff88add6ceafd02e4577d053138c20
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-11-17 19:22:52 -0500

    fix(eslintrc): turns off lint rules that break ui/chart.tsx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,11 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/consistent-indexed-object-style": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
